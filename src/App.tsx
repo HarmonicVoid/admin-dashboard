4d0f43c427343e8b373f388586850894797cca6a
HarmonicVoid <marvin.bolanos25@gmail.com>
2023-10-05 21:39:49 -0700

    feat: adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Home from './Pages/Home/home';
+import Users from './Pages/users/Users';
+import Products from './Pages/products/Products';
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: 'users',
+      element: <Users />,
+    },
+    {
+      path: 'products',
+      element: <Products />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
